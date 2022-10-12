0e13e71e4605645325cf2d6a0e8035c8fb68b478
fellipeutaka <fellipeutaka@gmail.com>
2022-10-12 19:25:50 -0300

    ci: Changed viteFinal to Github Pages

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,7 +15,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
-  async viteFinal(config) {
+  async viteFinal(config, { configType }) {
+    if (configType === "PRODUCTION") {
+      config.base = "/iglab-design-system/";
+    }
+
     return mergeConfig(config, {
       resolve: {
         alias: [
